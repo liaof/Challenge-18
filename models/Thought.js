e880557f6f5276832c772c919876a1751c2722e5
liaof <f.liao163@gmail.com>
2021-06-26 21:13:36 -0400

    models added

--- models/Thought.js
+++ models/Thought.js
@@ -1,39 +1,68 @@
  const { Schema, model } = require('mongoose');
 
- const ThoughtSchema = new Schema(
-     {
-         thoughtText: {
+
+
+const ReactionSchema = new Schema(
+    {
+         reactionID: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+         },
+         reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+         },
+         username: {
              type: String,
              required: true,
-             minLength: 1,
-             maxLength: 180
+             trim: true
          },
          createdAt: {
             type: Date,
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
-         },
-         username: {
-             type: String,
-             required: true
-         },
-         reactions: [ReactionSchema]
-     },
- )
-
-const ReactionSchema = new Schema(
-     {
-         reactionID: {
-
-         },
-         reactionBody: {
-
-         },
-         username: {
-
-         },
-         createdAt: {
-
          }
-     }
-)
+     },
+     {
+        toJSON: {
+          getters: true
+        }
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+           type: Date,
+           default: Date.now,
+           get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+   },
+   {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
